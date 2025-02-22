1421df03838974eec28f39c1049469de92f6d1ab
WaterLeafKing <45875396+WaterLeafKing@users.noreply.github.com>
2025-02-22 15:58:19 +0900

    fix:next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,35 @@ import removeImports from 'next-remove-imports';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@uiw/react-md-editor'],
+  webpack: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+            include: [/@uiw\/react-md-editor\//],
+          },
+        ],
+      },
+    };
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap.xml',
+      },
+      {
+        source: '/robots.txt',
+        destination: '/api/robots.txt',
+      },
+    ];
+  },
 };
 
 export default removeImports(nextConfig);
